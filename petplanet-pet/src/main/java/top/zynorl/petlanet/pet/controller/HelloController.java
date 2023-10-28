039f2c52bfff936e1311ccce8356ecfb1a72f75b
nbsuipeng <1547837674@qq.com>
2023-10-28 15:19:45 +0800

    调通gateway

---
+++ petplanet-pet/src/main/java/top/zynorl/petlanet/pet/controller/HelloController.java
@@ -0,0 +1,13 @@
+package top.zynorl.petlanet.pet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello()
+    {
+        return "hello";
+    }
+}
