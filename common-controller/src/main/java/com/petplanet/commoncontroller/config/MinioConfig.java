277a1426153f0921c1221bff3eb52c7d3215b00b
suipeng <1547837674@qq.com>
2023-12-16 22:46:34 +0800

    增加minio文件管理接口

---
+++ common-controller/src/main/java/com/petplanet/commoncontroller/config/MinioConfig.java
@@ -0,0 +1,49 @@
+package com.petplanet.commoncontroller.config;
+
+
+import io.minio.MinioClient;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * minio的配置类，用来关联application中的配置
+ */
+@Data
+@Configuration
+public class MinioConfig {
+
+    /**
+     * 访问地址
+     */
+    @Value("${minio.endpoint}")
+    private String endpoint;
+
+    /**
+     * accessKey类似于用户ID，用于唯一标识你的账户
+     */
+    @Value("${minio.accessKey}")
+    private String accessKey;
+
+    /**
+     * secretKey是你账户的密码
+     */
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+    /**
+     * 默认存储桶
+     */
+    @Value("${minio.bucketName}")
+    private String bucketName;
+
+    @Bean
+    public MinioClient minioClient() {
+        MinioClient minioClient = MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+        return minioClient;
+    }
+}
---
+++ common-controller/src/main/java/com/petplanet/commoncontroller/file/FileUpload.java
@@ -0,0 +1,106 @@
+package com.petplanet.commoncontroller.file;
+
+
+import com.petplanet.commoncontroller.config.MinioConfig;
+import com.petplanet.commoncontroller.utils.MinioUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+
+/**
+ * 负责上传文件到minio
+ */
+@Slf4j
+@RestController
+@RequestMapping("/file")
+public class FileUpload {
+
+
+    @Autowired
+    private MinioUtils minioUtils;
+
+    @Autowired
+    private MinioConfig minioConfig;
+
+    /**
+     * 文件上传
+     *
+     * @param file
+     */
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        try {
+            //文件名
+            String fileName = file.getOriginalFilename();
+            String newFileName = System.currentTimeMillis() + "." + StringUtils.substringAfterLast(fileName, ".");
+            //类型
+            String contentType = file.getContentType();
+            minioUtils.uploadFile(minioConfig.getBucketName(), file, newFileName, contentType);
+            String presignedObjectUrl = minioUtils.getPresignedObjectUrl(minioConfig.getBucketName() , newFileName);
+            return "上传成功:"+presignedObjectUrl;
+        } catch (Exception e) {
+            log.error("上传失败");
+            return "上传失败";
+        }
+    }
+
+    /**
+     * 删除
+     *
+     * @param fileName
+     */
+    @DeleteMapping("/delete")
+    public void delete(@RequestParam("fileName") String fileName) {
+        minioUtils.removeFile(minioConfig.getBucketName(), fileName);
+    }
+
+    /**
+     * 获取文件信息
+     *
+     * @param fileName
+     * @return
+     */
+    @GetMapping("/info")
+    public String getFileStatusInfo(@RequestParam("fileName") String fileName) {
+        return minioUtils.getFileStatusInfo(minioConfig.getBucketName(), fileName);
+    }
+
+    /**
+     * 获取文件外链
+     *
+     * @param fileName
+     * @return
+     */
+    @GetMapping("/url")
+    public String getPresignedObjectUrl(@RequestParam("fileName") String fileName) {
+        return minioUtils.getPresignedObjectUrl(minioConfig.getBucketName(), fileName);
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param fileName
+     * @param response
+     */
+    @GetMapping("/download")
+    public void download(@RequestParam("fileName") String fileName, HttpServletResponse response) {
+        try {
+            InputStream fileInputStream = minioUtils.getObject(minioConfig.getBucketName(), fileName);
+            response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+            response.setContentType("application/force-download");
+            response.setCharacterEncoding("UTF-8");
+            IOUtils.copy(fileInputStream, response.getOutputStream());
+        } catch (Exception e) {
+            log.error("下载失败");
+        }
+    }
+
+
+}
