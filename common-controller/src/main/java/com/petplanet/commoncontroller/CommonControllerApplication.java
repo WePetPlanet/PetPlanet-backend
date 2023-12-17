58c5426ba8160df1d48902c1c9ab59c1574c1b60
suipeng <1547837674@qq.com>
2023-12-17 19:54:26 +0800

    添加pom

---
+++ common-controller/src/main/java/com/petplanet/commoncontroller/CommonControllerApplication.java
@@ -0,0 +1,13 @@
+package com.petplanet.commoncontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonControllerApplication.class, args);
+    }
+
+}
---
+++ common-controller/src/test/java/com/petplanet/commoncontroller/CommonControllerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.petplanet.commoncontroller;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
