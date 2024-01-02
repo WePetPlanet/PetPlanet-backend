622a28801ddf25d0ec9206c8fbb031f4d06a2014
suipeng <1547837674@qq.com>
2024-01-02 22:42:12 +0800

    添加redis依赖

--- petplanet-gateway/src/main/java/top/zynorl/petplanet/gateway/controller/TestController.java
+++ petplanet-gateway/src/main/java/top/zynorl/petplanet/gateway/controller/TestController.java
@@ -1,15 +1,25 @@
 package top.zynorl.petplanet.gateway.controller;
 
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import top.zynorl.petplanet.gateway.req.LoginReq;
+
+import java.net.http.HttpRequest;
 
 /**
  * 测试类
  */
 @RestController
 public class TestController {
+    @RequestMapping("/session/get")
+    public Mono<ServerHttpRequest> sessionGet(ServerWebExchange exchange) {
+        ServerHttpRequest request = exchange.getRequest();
+        return Mono.just(request);
+    }
     @RequestMapping("/user/normal")
     public Mono<String> normal(LoginReq loginReq) {
         return Mono.just("/user/normal");
--- petplanet-pet/src/main/java/top/zynorl/petlanet/pet/controller/HelloController.java
+++ petplanet-pet/src/main/java/top/zynorl/petlanet/pet/controller/HelloController.java
@@ -3,11 +3,14 @@ package top.zynorl.petlanet.pet.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
-    public String hello()
+    public String hello(HttpSession session)
     {
-        return "hello";
+        System.out.println(session);
+        return "Pet module hello";
     }
 }
